2fc925742661d80141f37d5c93f2ac98de35edeb
kuroroLuciferPhantom <91409971+kuroroLuciferPhantom@users.noreply.github.com>
2025-03-02 10:01:45 +0100

    Fix database package index file

---
+++ packages/database/src/index.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+
+export * from '@prisma/client';
+
+// Add prisma to the NodeJS global type
+interface CustomNodeJsGlobal extends NodeJS.Global {
+  prisma: PrismaClient;
+}
+
+// Prevent multiple instances of Prisma Client in development
+declare const global: CustomNodeJsGlobal;
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
